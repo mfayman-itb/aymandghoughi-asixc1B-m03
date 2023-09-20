169033751dfd6ff60f6124c9ac54d2dd5c143247
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-09-20 13:01:25 +0200

    python code example

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,5 +4,5 @@ M03 UF1
 14/09/2023
 Exemple de de codi en Python
 """
-#TODO Es pot fer servir per fer proves amb el GIT
-print("Hello World!")
+# TODO Es pot fer servir per fer proves amb el GIT
+print('Hello World!')
