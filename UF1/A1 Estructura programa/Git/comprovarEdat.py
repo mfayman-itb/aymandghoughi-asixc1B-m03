8827212798ab8512107d6e105308a10e850c331f
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-09-21 11:57:42 +0200

    Pr1 Estructura d'un programa informàtic

---
+++ UF1/A1 Estructura programa/Git/comprovarEdat.py
@@ -0,0 +1,11 @@
+"""
+Ayman Dghoughi Nouri
+21/09/2023
+ASIXc M03 UF1 A1
+Descripció: Programa que demana l'edat i diu si ets major d'edat.
+"""
+edat = int(input("Quina edat tens?"))
+if edat >= 18:
+    print("Ets major d'edat")
+
+print("Programa finalitzat")
