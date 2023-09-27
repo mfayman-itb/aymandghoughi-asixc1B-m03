921cfa41fbdcb82e1eeca557c56b91d1be647ac0
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-09-27 12:42:49 +0200

    comprovarEdat updated and optimized

--- UF1/A1 Estructura programa/Git/comprovarEdat.py
+++ UF1/A1 Estructura programa/Git/comprovarEdat.py
@@ -4,8 +4,12 @@ Ayman Dghoughi Nouri
 ASIXc M03 UF1 A1
 Descripció: Programa que demana l'edat i diu si ets major d'edat.
 """
-edat = int(input("Quina edat tens?"))
-if edat >= 18:
-    print("Ets major d'edat")
-
-print("Programa finalitzat")
+try:
+    edat = int(input("Quina edat tens? "))
+    if edat >= 18:
+        print("Ets MAJOR d'edat")
+    else:
+        print("Ets MENOR d'edat")
+    print("Programa finalitzat")
+except ValueError:
+    print("ERROR: Valor no valid")
