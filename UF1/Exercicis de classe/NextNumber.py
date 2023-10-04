a7e84801eeff9fae87fa4cacadbebbeead885dd4
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-10-04 12:16:41 +0200

    Activitat M03 UF1

--- UF1/Exercicis de classe/NextNumber.py
+++ UF1/Exercicis de classe/NextNumber.py
@@ -5,9 +5,8 @@ M03 UF1
 Descripció: L'usuari escriu un número enter. "Printa" per pantalla el número següent.
 Per exemple, si entra el 4. El programa ha de mostrar la frase "Després ve el 5".
 """
-
 try:
-    nbr = int(input())
+    nbr = int(input("Introdueix un valor: "))
     nxt_nbr = nbr + 1
     print("Després ve el ", str(nxt_nbr))
     print("Program completed")
