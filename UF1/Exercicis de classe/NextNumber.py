7b4b8c0ab5cf7ee55f49a6f0665c9185ee153f0c
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-10-04 12:13:49 +0200

    NextNumber completed

---
+++ UF1/Exercicis de classe/NextNumber.py
@@ -0,0 +1,16 @@
+"""
+Ayman Dghoughi Nouri
+M03 UF1
+4/10/2023
+Descripció: L'usuari escriu un número enter. "Printa" per pantalla el número següent.
+Per exemple, si entra el 4. El programa ha de mostrar la frase "Després ve el 5".
+"""
+
+try:
+    nbr = int(input())
+    nxt_nbr = nbr + 1
+    print("Després ve el ", str(nxt_nbr))
+    print("Program completed")
+except Exception as e:
+    print(e)
+    exit(1)
