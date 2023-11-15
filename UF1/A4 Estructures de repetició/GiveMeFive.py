2a0e842228cc245ad441ca8471acf3d7fb11dbca
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-11-15 12:57:13 +0100

    goooood

---
+++ UF1/A4 Estructures de repetició/GiveMeFive.py
@@ -0,0 +1,4 @@
+number = 0
+while number != 5 and number != 1234:
+    number = int(input("Give me a five: "))
+print("Great thank tou <3")
---
+++ UF1/A4 Estructures de repetició/isPinOk.py
@@ -0,0 +1,15 @@
+x = 1
+PIN = 1234
+EASTER_EGG = "0007"
+
+clave = input("Dime la clave: ")
+while int(clave) != PIN and clave != EASTER_EGG and x != 3:
+    print("Clave incorrecta")
+    clave = input("Dime la clave: ")
+    x = x + 1
+if x == 3:
+    print("Intents esgotats: LAS CAGAO BACALAO")
+elif int(clave) == PIN:
+    print("PIN correcte has fet servir " + str(x) + " intents")
+elif clave == EASTER_EGG:
+    print("EASTER EGG correcte has fet servir " + str(x) + " intents")
