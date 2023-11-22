3ce49e647d3786bdeacf52ce7b295d77ecdb7538
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-11-22 13:02:52 +0100

    goooood

---
+++ UF1/A4 Estructures de repetició/DotLIne.py
@@ -0,0 +1,6 @@
+line = int(input("GIve me the number of lines: "))
+column = int(input("Give me the number of columns: "))
+x = "."
+for i in range(line):
+    print(x)
+print(x * 10)
---
+++ UF1/A4 Estructures de repetició/LetsCount.py
@@ -0,0 +1,5 @@
+num = int(input("Give me a number: "))
+x = 1
+for i in range(num):
+    print(i, end=" ")
+    x += 1
