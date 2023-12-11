38f545fde544fe083a646a60b4f52c90159057fc
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2023-12-11 12:09:20 +0100

    goooood

---
+++ UF1/A4 Estructures de repetició/e8.py
@@ -0,0 +1,24 @@
+limI = int(input())
+limS = int(input())
+suma = 0
+fora = 0
+enLim = False
+
+while limI > limS:
+    limI = int(input())
+    limS = int(input())
+
+num = int(input())
+while num != 0:
+    num = int(input())
+    if num > limI and num < limS:
+        suma += num
+    elif num < limI or num > limS
+        fora += 1
+    elif num == limI or num == limS:
+        enLim = True
+print("suma de fora")
+if enLim == True:
+    print()
+else:
+    print()
