b31f81f2bb85e69874000d9be921fd2d7d8e4d15
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2024-01-24 11:21:46 +0100

    goooood

--- UF1/A5 Tipus de dades Sequència i Diccionari/randomStudent.py
+++ UF1/A5 Tipus de dades Sequència i Diccionari/randomStudent.py
@@ -1,7 +1,6 @@
 import random
 
-CORREO = (
-    "tamoor.asghar.7e7@itb.cat",
+CORREO = ("tamoor.asghar.7e7@itb.cat",
     "xavier.cabello.7e7@itb.cat",
     "oriol.canellas.7e3@itb.cat",
     "ayman.dghoughi.7e7@itb.cat",
@@ -30,8 +29,52 @@ CORREO = (
     "pol.sanchez.7e7@itb.cat",
     "taranpreet.singh.7e7@itb.cat",
     "rene.tubiera.7e7@itb.cat",
-    "jordi.yu.7e7@itb.cat"
-)
-
+    "jordi.yu.7e7@itb.cat")
 pick = random.randint(0, len(CORREO) - 1)
 print(CORREO[pick])
+
+"""
+import turtle
+import random
+
+# Configuración inicial
+turtle.speed(0)
+turtle.bgcolor("white")
+turtle.title("Ruleta")
+
+# Colores
+colores = ["red", "black", "green", "blue", "yellow", "orange", "purple", "pink"]
+
+# Función para dibujar una caja de sección
+def dibujar_seccion(color, tamaño):
+    turtle.color(color)
+    turtle.begin_fill()
+    turtle.circle(100, tamaño)
+    turtle.forward(40)
+    turtle.left(90)
+    turtle.forward(20)
+    turtle.left(90)
+    turtle.forward(40)
+    turtle.left(90)
+    turtle.forward(20)
+    turtle.left(90)
+    turtle.end_fill()
+
+# Dibujar las secciones de la ruleta
+for _ in range(8):
+    color = random.choice(colores)
+    tamaño = random.randint(10, 60)
+    dibujar_seccion(color, tamaño)
+
+# Dibujar el indicador central
+turtle.penup()
+turtle.goto(0, -30)
+turtle.pendown()
+turtle.color("black")
+turtle.begin_fill()
+turtle.circle(10)
+turtle.end_fill()
+
+# Mostrar la ventana de la ruleta
+turtle.done()
+"""
