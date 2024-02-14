63bbb7c98f60b67d0b9dfb4843dd9a6a8bd32d1b
Ayman Dghoughi Nouri <145555412+mfayman-itb@users.noreply.github.com>
2024-02-14 13:02:57 +0100

    Update MatchAnalizer.py

--- UF2/A1 Disseny descendent i Parametrització/MatchAnalizer.py
+++ UF2/A1 Disseny descendent i Parametrització/MatchAnalizer.py
@@ -1,17 +1,31 @@
 #region Functions
-def teams():
-    team1 = input()
-    team2 = input()
+teams = []
+points = []
 
-def points():
-    points = []
-    while points != '-1':
-        if len(points) == 2:
-            points = input().split()
-        else:
-            print("Introdueix 2 numeros")
+def what_teams():
+    t1 = input('local team: ')
+    t2 = input('visitor team: ')
+    return t1,t2
+def obtain_points():
+    scores = []
+    while True:
+        oldscores = scores
+        scores = input("Introduce the result (or -1 to finish): ").split()
+        if '-1' in scores:
+            break
+        if len(scores) != 2:
+            print("Please enter exactly 2 scores separated by space.")
+            continue
+        if not all(score.isdigit() for score in scores):
+            print("Please enter only integers.")
+            continue
+        points.append((int(scores[0]), int(scores[1])))
+    return points
+print(points)
+def compare_points():
 
+def winner():
 
 #endregion
-teams()
-points()
+what_teams()
+obtain_points()
