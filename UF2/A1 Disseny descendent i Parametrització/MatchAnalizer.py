0919235a8b511158ef3a90edbe7377cea0be52f7
Ayman Dghoughi Nouri <145555412+mfayman-itb@users.noreply.github.com>
2024-02-15 01:04:35 +0100

    Update MatchAnalizer.py

--- UF2/A1 Disseny descendent i Parametrització/MatchAnalizer.py
+++ UF2/A1 Disseny descendent i Parametrització/MatchAnalizer.py
@@ -23,9 +23,38 @@ def obtain_points():
     return points
 print(points)
 def compare_points():
+        for score in points:
+        if score[0] == 1:
+            print("Cistella de", teams[0])
+        elif score[0] == 2:
+            print("Doble de", teams[0])
+        elif score[0] == 3:
+            print("Triple de", teams[0])
+        else:
+            print("Tir lliure de", teams[0])
 
+        if score[1] == 1:
+            print("Cistella de", teams[1])
+        elif score[1] == 2:
+            print("Doble de", teams[1])
+        elif score[1] == 3:
+            print("Triple de", teams[1])
+        else:
+            print("Tir lliure de", teams[1])
 def winner():
-
+    score_team_1 = sum([score[0] for score in points])
+    score_team_2 = sum([score[1] for score in points])
+    
+    if score_team_1 > score_team_2:
+        print("Guanya", teams[0])
+    elif score_team_2 > score_team_1:
+        print("Guanya", teams[1])
+    else:
+        print("Empat")
 #endregion
-what_teams()
+t1, t2 = what_teams()
+teams.append(t1)
+teams.append(t2)
 obtain_points()
+compare_points()
+winner()
