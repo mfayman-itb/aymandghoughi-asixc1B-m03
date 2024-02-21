dca0f652131a54f6ba3bf747976ba619453e9287
ayman <ayman.dghoughi.7e7@itb.cat>
2024-02-21 12:17:27 +0100

    updt

--- UF2/A1 Disseny descendent i Parametrització/LinuxCommands.py
+++ UF2/A1 Disseny descendent i Parametrització/LinuxCommands.py
@@ -1,40 +1,62 @@
-#region variables i constants
+#region variables
+import random
 CMD = ('touch', 'grep', 'cat', 'fdisk', 'cmp', 'dmesg', 'man', 'top', 'htop', 'halt')
 OPTIONS = ['-v', '-h', '--version', '--help']
 LACABRA = (('v1.2', 'touch: Create an empty file.'), ('v2.5', 'grep: Search for patterns in files.'), ('v3.1', 'cat: Concatenate and display file contents.'), ('v1.8', 'fdisk: Manipulate disk partition tables.'), ('v2.3', 'cmp: Compare two files byte by byte.'), ('v1.6', 'dmesg: Display system message buffer.'), ('v2.0', 'man: Display manual pages for commands.'), ('v1.4', 'top: Display and update sorted information about processes.'), ('v3.2', 'htop: Interactive process viewer.'), ('v1.9', 'halt: Stop the system.'))
 #endregion
 
 #region functions
+def get_random_greeting():
+    greetings = [
+        "¡Hola! ¿En qué puedo ayudarte hoy?",
+        "Bienvenido al nuevo sistema operativo en desarrollo.",
+        "¿Cómo puedo servirte?",
+        "¡Saludos! ¿Qué necesitas hacer?"
+    ]
+    return random.choice(greetings)
 def cmd_input(CMD, OPTIONS):
     try:
-        comand = [0]
-        while comand[0] != 'halt':
+        print(get_random_greeting())
+        running = True
+        while running:
             comand = input().split()
-            if len(comand) != 1:
-                if comand[0] in CMD and comand[1] in OPTIONS:
-                    if comand[1] == '--version':
-                        comand.pop(-1)
-                        comand.append('-v')
+            if len(comand) >= 1:
+                if comand[0] in CMD:
+                    if len(comand) == 1:
+                        if comand[0] == 'halt':
+                            print('Halting the system...')
+                            running = False  # Se detiene el bucle
+                        else:
+                            print(f'{comand[0]} needs one parameter. For instance -v or -h')
+                            continue
                     else:
-                        comand.pop(-1)
-                        comand.append('-h')
-                    OPTIONS.pop(-1)
-                    OPTIONS.pop(-1)
-                    cmd_exe(comand)
-                elif comand[0] in CMD and comand[1] not in OPTIONS:
-                    print(f'Value {comand[1]} is not valid option')
+                        option = comand[1]
+                        if option in OPTIONS:
+                            if option == '--version':
+                                option = '-v'
+                            elif option == '--help':
+                                option = '-h'
+                            elif option == '-v' or option == '-h':
+                                pass
+                            else:
+                                print(f'Invalid option: {option}')
+                                continue
+                        else:
+                            print(f'Invalid option: {option}')
+                            continue
+                        comand[1] = option
+                        cmd_exe(comand)
                 else:
-                    print(f'{comand} command not found.')
-            elif comand[0] != 'halt' and comand[0] in CMD:
-                print(f'{comand[0]} needs one paramater. For instance -v or -h')
+                    print(f'{comand[0]} command not found.')
             else:
-                print(f'need options')
+                print(f'Need command')
     except Exception as e:
         print(e)
 def cmd_exe(comand):
     fnc = CMD.index(comand[0])
     opt = OPTIONS.index(comand[1])
     print(LACABRA[fnc][opt])
+#endregion
 
 #region main
 cmd_input(CMD, OPTIONS)
