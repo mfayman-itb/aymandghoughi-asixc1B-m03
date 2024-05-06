ac0c68a1e951ea5b5ebc8f0d95ffd30f08e740dd
mfayman-itb <ayman.dghoughi.7e7@itb.cat>
2024-05-06 23:03:06 +0200

    new shi

---
+++ UF3/A1 Fonaments de gestió de Ditxers/lamp/lamp_actions.py
@@ -0,0 +1,14 @@
+def turn_on(light):
+    light = True
+    print(light)
+    return light
+
+def turn_off(light):
+    light = False
+    print(light)
+    return light
+
+def toggle(light):
+    light = not light
+    print(light)
+    return light
