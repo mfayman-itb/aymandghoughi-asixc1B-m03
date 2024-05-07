8af55c6553898cfc15126a373569a0c0508a5152
Ayman Dghoughi Nouri <145555412+mfayman-itb@users.noreply.github.com>
2024-05-07 10:26:01 +0200

    Delete UF3/A1 Fonaments de gestió de Ditxers directory

--- UF3/A1 Fonaments de gestió de Ditxers/cheapestPrice/main.py
+++
@@ -1,9 +0,0 @@
-from cheap import *
-
-def main():
-    preus = see_the_cheapest()
-    preu_mes_baix = trobar_preu_mes_baix(preus)
-    mostrar_preu_mes_baix(preu_mes_baix)
-
-if __name__ == "__main__":
-    main()
